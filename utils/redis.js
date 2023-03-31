015407cee3481cfad73ef8cc6e567dd05d207990
“victornnamdii” <victorilodiuba@gmail.com>
2023-03-31 13:08:15 +0100

    Task 0

---
+++ utils/redis.js
@@ -0,0 +1,37 @@
+/* eslint-disable no-unused-vars */
+import { createClient } from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+    this.clientConnected = true;
+    this.client.on('error', (err) => {
+      console.log(err.toString());
+      this.clientConnected = false;
+    });
+    this.client.on('connect', () => {
+      this.clientConnected = true;
+    });
+  }
+
+  isAlive() {
+    return this.clientConnected;
+  }
+
+  async get(key) {
+    return promisify(this.client.get).bind(this.client)(key);
+  }
+
+  async set(key, value, duration) {
+    await promisify(this.client.SETEX).bind(this.client)(key, duration, value);
+  }
+
+  async del(key) {
+    await promisify(this.client.DEL).bind(this.client)(key);
+  }
+}
+
+const redisClient = new RedisClient();
+
+module.exports = redisClient;
